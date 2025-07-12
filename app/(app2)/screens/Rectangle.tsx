41ea16c903fb1ab90de757372d08034ab8170b88
161-Keshav <ppg.gnanakeshav@gmail.com>
2025-07-12 17:36:29 +0530

    Initial Tab Structure

--- app/(app2)/screens/Rectangle.tsx
+++ app/(app2)/screens/Rectangle.tsx
@@ -11,23 +11,3 @@ const Rectangle = ({ children }: { children: React.ReactNode }) => {
 
 export default Rectangle;
 
-
-// import * as React from "react";
-// import {StyleSheet, View} from "react-native";
-// const Rectangle = ({ children }: { children: React.ReactNode }) => {
-//     return (
-//         <View style={styles.rectangleView} />);
-// };
-// const styles = StyleSheet.create({
-//     rectangleView: {
-//         borderRadius: 22,
-//         backgroundColor: "#f6e5e5",
-//         borderStyle: "solid",
-//         borderColor: "#4040a6",
-//         borderWidth: 7,
-//         flex: 1,
-//         width: "100%",
-//         height: 252
-//     }
-// });
-// export default Rectangle;
